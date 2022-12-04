9c130b591c66e385411a93d7fd955a6f477d6676
manh nguyen <manhnguyen16112@gmail.com>
2022-12-04 15:09:48 +0700

    remove file unused

--- src/main/java/sokhoahoccongnghe/phutho/gov/vn/repository/FileTypeRepository.java
+++
@@ -1,9 +0,0 @@
-//package sokhoahoccongnghe.phutho.gov.vn.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface FileTypeRepository extends JpaRepository<FileType,Integer> {
-//    FileType findFirstByName(String name);
-//}
